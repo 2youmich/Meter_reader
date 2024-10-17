733bab4bb9371bdba750a0d726c871dad83380dc
Jonathan Snyder <jon.jsstudios@gmail.com>
2024-10-17 20:06:45 +0700

    fix css changes

--- src/App.vue
+++ src/App.vue
@@ -69,12 +69,17 @@ nav a:first-of-type {
   border: 0;
 }
 
+/*
+ */
+
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
+
+  /* test comment */
 
   .logo {
     margin: 0 2rem 0 0;
